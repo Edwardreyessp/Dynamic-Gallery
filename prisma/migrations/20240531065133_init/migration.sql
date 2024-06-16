64cd0856a933322d139ffc4bb7bdcb7bbb84223f
Edward Daniel Reyes Pérez <edwardreyessp@gmail.com>
2024-06-15 21:31:12 -0600

    refactor(database): Prisma migration

--- prisma/migrations/20240531065133_init/migration.sql
+++ prisma/migrations/20240531065133_init/migration.sql
@@ -4,12 +4,8 @@ CREATE TABLE "Photo" (
     "url" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "tags" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Photo_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Photo_slug_key" ON "Photo"("slug");
